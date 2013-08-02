2797bd4c438d0d6b3b7c851b7ea60f9c96d17968
suptaphilip <supta.philip@gmail.com>
2013-08-02 13:14:31 +0200

    python bam bam!

--- Day1/first.py
+++ Day1/first.py
@@ -2,3 +2,29 @@
 #This program is written by Supta Richard Philip
 
 print "Hello Richard from Eclipse!"
+
+# Add two numbers
+print  3+4
+
+# Substraction
+print 4-2
+
+#Multiplication
+print 3*3
+
+#Division
+print 4/2
+
+#Modulas
+print 3%2
+
+#exponential
+print 4**2
+
+# add two numbers
+x= 12
+y = 12.2
+
+print x+y
+
+
